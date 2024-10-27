931aa5716a069e9b166c8efac622e202dc22a682
Robert Fjellin <155790151+robfjellin@users.noreply.github.com>
2024-10-27 17:04:54 -0400

    Delete build directory

--- build/Qt_6_8_0_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BoomCalculator_FILE /Users/rob/Development/Qt/BoomCalculator/build/Qt_6_8_0_for_macOS-Debug/BoomCalculator.app/Contents/MacOS/BoomCalculator)
-set(__QT_DEPLOY_TARGET_BoomCalculator_TYPE EXECUTABLE)
